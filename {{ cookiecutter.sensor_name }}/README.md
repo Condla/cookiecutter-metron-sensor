b232a7312e138bd319eda6a223620f3e2c6ba5a5
Stefan Kupstaitis-Dunkler <stefan.dun@gmail.com>
2019-02-09 01:08:22 +0000

    add capability to configure streaming enrichment

--- {{ cookiecutter.sensor_name }}/README.md
+++ {{ cookiecutter.sensor_name }}/README.md
@@ -46,43 +46,37 @@ or:
 
 * Then, check all files that have been created and change anything to fit your needs:
 
-    * add fields to the Elastic Search template in `elastic.json`
+    * Add fields to the Elastic Search template in `elastic.json`
 
-    * change Storm settings based on the number of expected event in the `parser.json`
+    * If you use Grok, develop your Grok statement and put it into the Grok file
 
-    * add specific enrichments if required to the `enrichment.json`
+    * add specific enrichments, if required to the `enrichment.json`
 
-    * configure the `batchSize` according to your throughput in the `
-
-    * 
+* Make sure that the user executing the pipelines, which is usually the `metron` user, has sufficient permissions to read/write to the Kafka topic `{{ cookiecutter.kafka_topic_name }}` and has full permissions to manage the Storm topology `{{ cookiecutter.sensor_name }}`. Apache Ranger is a great tool to do this.
 
 * Then, set the following environment variables
 
 ```bash
 export SENSOR="{{ cookiecutter.sensor_name }}"
-export ELASTIC_USER="elastic"
+export ELASTIC_USER="{{ cookiecutter.elastic_user }}"
 export ELASTIC_PASSWORD="<password>"
-export ELASTICMASTER="https://elastic.fqdn.com:9200"
-export METRON_REST_USER="metron"
+export ELASTICMASTER="{{ cookiecutter.elastic_master }}"
+export METRON_REST_USER="{{ cookiecutter.metron_user }}"
 export METRON_REST_PASSWORD="<password>"
-export METRON_REST_URL="https://metron.fqdn.com:8082"
+export METRON_REST_URL="{{ cookiecutter.metron_rest }}"
 ```
 
-* Deploy by executing the `deploy.sh` script.
+* Deploy by executing the `deploy.sh` script to upload and deploy all configuration files:
 
 ```
 bash deploy.sh
 ```
 
-This will upload all required file 
-
 * Check in the Metron Management UI if everything was posted correctly.
-
-* Make sure that the user executing the pipelines, which is usually the `metron` user, has sufficient permissions to read/write to the Kafka topic `{{ cookiecutter.kafka_topic_name }}` and has full permissions to manage the Storm topology `{{ cookiecutter.sensor_name }}`
 
 * Start the sensor from the Metron Management UI by clicking the "play" button next to the `{{ cookiecutter.sensor_name }}`` sensor.
 
-* Push sample events into Kafka and test the pipeline.
+* Push sample events into Kafka and test the pipeline:
 
 ```bash
 /usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list broker1.kafka:6667 --security-protocol SASL_PLAINTEXT --topic {{ cookiecutter.kafka_topic_name }}
--- {{ cookiecutter.sensor_name }}/deploy.sh
+++ {{ cookiecutter.sensor_name }}/deploy.sh
@@ -2,20 +2,37 @@
 
 export SENSOR=${1:-"{{ cookiecutter.sensor_name }}"}
 
-echo "Deploy ES template:"
+echo "########      Deploy ES template       #########"
+{% if cookiecutter.elastic_user == "" %}
+curl -X POST $ELASTICMASTER/_template/${SENSOR}_index -d @elastic.json
+{% else %}
 curl -u $ELASTIC_USER:$ELASTIC_PASSWORD -X POST $ELASTICMASTER/_template/${SENSOR}_index -d @elastic.json
+{% endif %}
+echo \n\n
 
-echo "Deploy parser config:"
+echo "########      Deploy parser config     #########"
 curl -iv -u $METRON_REST_USER:$METRON_REST_PASSWORD -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @parser.json $METRON_REST_URL/api/v1/sensor/parser/config/$SENSOR
+echo \n\n
 
-echo "Deploy enrichment config:"
+
+echo "########      Deploy enrichment config #########"
 curl -iv -u $METRON_REST_USER:$METRON_REST_PASSWORD -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @enrichment.json $METRON_REST_URL/api/v1/sensor/enrichment/config/$SENSOR
+echo \n\n
 
-echo "Deploy indexing config:"
+
+echo "########      Deploy indexing config   #########"
 curl -iv -u $METRON_REST_USER:$METRON_REST_PASSWORD -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @indexing.json $METRON_REST_URL/api/v1/sensor/indexing/config/$SENSOR
+echo \n\n
 
-echo "Deploy Grok statement"
-hdfs dfs -put grok /apps/metron/patterns/$SENSOR || echo "No Grok statement present."
 
-echo "Create Kafka topic"
-curl -iv -u $METRON_REST_USER:$METRON_REST_PASSWORD -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @kafka.json $METRON_REST_URL/api/v1/kafka/topic
+{% if cookiecutter.parser_class_name=="org.apache.metron.parsers.GrokParser" %}
+echo "########      Deploy Grok statement    #########"
+sudo su metron -c "kinit -kt /etc/security/keytabs/metron.headless.keytab metron"
+sudo su metron -c "hdfs dfs -put grok /apps/metron/patterns/$SENSOR"
+echo \n\n
+{% endif %}
+
+echo "########      Create Kafka topic       #########"
+sudo su kafka -c "/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper {{ cookiecutter.zookeeper_quorum }} --create --topic {{ cookiecutter.kafka_topic_name }} --partitions {{ cookiecutter.kafka_number_partitions }} --replication-factor {{ cookiecutter.kafka_number_replicas }}"
+#curl -iv -u $METRON_REST_USER:$METRON_REST_PASSWORD -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d @kafka.json $METRON_REST_URL/api/v1/kafka/topic
+echo \n\n
